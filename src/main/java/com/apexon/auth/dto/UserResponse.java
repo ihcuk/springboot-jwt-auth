2b0594c2ef309708b67945fb0077e5248da5f4ff
ihcuk <shashank130.48@gmail.com>
2025-07-18 11:41:34 +0530

    Fix Changes

--- src/main/java/com/apexon/auth/dto/UserResponse.java
+++ src/main/java/com/apexon/auth/dto/UserResponse.java
@@ -1,9 +1,13 @@
 package com.apexon.auth.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserResponse {
     private Long id;
